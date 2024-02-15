9e37066bb9f892331adc7c00a963d7b09fff7b39
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-15 11:19:36 +0600

    upload assets on scene

--- src/scenes/Main.scene.ts
+++ src/scenes/Main.scene.ts
@@ -16,10 +16,17 @@ export const useMainScene = () => {
 
     function preload ()
     {
+        this.load.image('sky', '/assets/backgrounds/sky.png');
+        this.load.image('ground', '/assets/objects/platform.png');
+        this.load.image('star', '/assets/objects/star.png');
+        this.load.image('bomb', '/assets/objects/bomb.png');
+        this.load.spritesheet('dude', '/assets/player/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
     function create ()
     {
+        this.add.image(400, 300, 'sky');
+        this.add.image(400, 300, 'star');
     }
 
     function update ()
