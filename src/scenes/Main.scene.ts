2483f323454b96e3203b2291ebd31ef9562f9eae
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-17 17:43:04 +0600

    add bombs

--- src/scenes/Main.scene.ts
+++ src/scenes/Main.scene.ts
@@ -125,6 +125,19 @@ export const useMainScene = () => {
         star.disableBody(true, true)
         score += 10;
         scoreText.setText('Score: ' + score);
+
+        if (stars.countActive(true) === 0) {
+            stars.children.iterate(function (child) {
+                child.enableBody(true, child.x, 0, true, true);
+            });
+
+            const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+            const bomb = bombs.create(x, 16, 'bomb');
+
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }
     }
 
     function hitBomb (player, bomb)
