87b11f2a2275e9c3fd3320cbb96e42cc685d31b5
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-19 22:41:36 +0600

    create player with idle animation

--- src/scenes/Level01.scene.ts
+++ src/scenes/Level01.scene.ts
@@ -1,7 +1,7 @@
 import * as Phaser from "phaser";
 
 class GameScene extends Phaser.Scene {
-    // private player!: Phaser.Physics.Arcade.Image & { body: Phaser.Physics.Arcade.Body }
+    private player!: Phaser.Physics.Arcade.Image & { body: Phaser.Physics.Arcade.Body }
     private platforms!: Phaser.Physics.Arcade.StaticGroup
 
     constructor() {
@@ -11,6 +11,7 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('sky', '/assets/backgrounds/01.png');
         this.load.image('ground01', '/assets/tiles/ground01.png');
+        this.load.spritesheet('player', '/assets/player/player.png', { frameWidth: 116, frameHeight: 108 });
     }
 
      create() {
@@ -20,10 +21,23 @@ class GameScene extends Phaser.Scene {
             repeat: 20,
             setXY: {x: 138/2, y: window.innerHeight - 138/2, stepX: 138},
         });
+
+         this.player = this.physics.add.sprite(450, 450, 'player')
+
+         this.physics.add.collider(this.player, this.platforms);
+
+         this.anims.create({
+             key: 'idle',
+             frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+             frameRate: 10,
+             repeat: -1
+         });
+
+
     }
 
      update() {
-
+         this.player.anims.play('idle', true);
      }
 }
 
