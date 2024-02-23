a541e80b1f00691b104a37348ad1154043545ba8
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-23 16:20:26 +0600

    create smoke effect

--- src/scenes/Level01.scene.ts
+++ src/scenes/Level01.scene.ts
@@ -7,6 +7,7 @@ class GameScene extends Phaser.Scene {
     private cursors!: Phaser.Input.Keyboard.KeyboardManager
     private map!: Phaser.Tilemaps.Tilemap
     private layer!: Phaser.Tilemaps.TilemapLayer
+    private smoke!: Phaser.Physics.Arcade.Image & { body: Phaser.Physics.Arcade.Body }
 
     constructor() {
         super();
@@ -19,6 +20,7 @@ class GameScene extends Phaser.Scene {
         this.load.atlas('player-fly', '/assets/player/player-fly.png', '/assets/player/player-fly.json');
         this.load.atlas('player-walk', '/assets/player/player-walk.png', '/assets/player/player-walk.json');
         this.load.atlas('player-jump', '/assets/player/player-jump.png', '/assets/player/player-jump.json');
+        this.load.atlas('jetpack-smoke', '/assets/player/jetpack-smoke.png', '/assets/player/jetpack-smoke.json');
         this.load.tilemapTiledJSON('tilemap', '/assets/tiles/LevelOneMap.json');
     }
 
@@ -27,6 +29,9 @@ class GameScene extends Phaser.Scene {
         const tileset = this.map.addTilesetImage('ground01', 'ground01') as Tileset;
         this.add.tileSprite(2346 / 2, 1119 / 2, 2346 * 3, 1119, 'sky').setScrollFactor(0.5, 0);
          this.player = this.physics.add.sprite(450, 450, 'player-idle').setSize(115, 108);
+         this.smoke = this.physics.add.sprite(this.player.x, this.player.y, 'jetpack-smoke');
+         this.smoke.allowGravity = false;
+         this.smoke.immovable = true;
          this.layer = this.map.createLayer('Ground', tileset) as TilemapLayer;
          this.map.setCollision([1]);
          this.cameras.main.startFollow(this.player);
@@ -37,6 +42,20 @@ class GameScene extends Phaser.Scene {
                  {
                      start: 1,
                      end: 4,
+                     zeroPad: 0,
+                     suffix: '.png'
+                 }
+             ),
+             frameRate: 10,
+             repeat: -1
+         });
+
+         this.anims.create({
+             key: 'jetpack-smoke',
+             frames: this.anims.generateFrameNames('jetpack-smoke',
+                 {
+                     start: 4,
+                     end: 1,
                      zeroPad: 0,
                      suffix: '.png'
                  }
@@ -85,33 +104,59 @@ class GameScene extends Phaser.Scene {
 
      update() {
          this.cursors = this.input.keyboard.createCursorKeys()
-
          this.physics.collide(this.player, this.layer);
+         this.smoke.y = this.player.y + 120;
+
+         if (this.player.flipX) {
+             this.smoke.x = this.player.x + 35;
+         } else {
+             this.smoke.x = this.player.x - 45
+         }
+
          if (this.cursors.up.isDown) {
              this.player.setVelocityY(-500);
+             this.smoke.setVelocityY(-500);
              this.player.anims.play('fly', true);
+             this.smoke.visible = true;
+             this.smoke.anims.play('jetpack-smoke', true);
          } else if (this.cursors.right.isDown && !this.player.body.blocked.down) {
              this.player.setVelocityX(300);
+             this.smoke.setVelocityX(300);
              this.player.flipX = false;
              this.player.anims.play('jump', true);
+             this.smoke.visible = false;
+             this.smoke.setVelocityY(0);
          } else if (this.cursors.right.isDown && this.player.body.blocked.down) {
-             this.player.setVelocityX(250);
+             this.player.setVelocityX(500);
+             this.smoke.setVelocityX(500);
              this.player.flipX = false;
              this.player.anims.play('walk', true);
+             this.smoke.visible = false;
+             this.smoke.setVelocityY(0);
          } else if (this.cursors.left.isDown && this.player.body.blocked.down) {
-             this.player.setVelocityX(-250);
+             this.player.setVelocityX(-500);
+             this.smoke.setVelocityX(-500);
              this.player.flipX = true;
              this.player.anims.play('walk', true);
+             this.smoke.visible = false;
+             this.smoke.setVelocityY(0);
          } else if (this.cursors.left.isDown && !this.player.body.blocked.down) {
              this.player.setVelocityX(-300);
+             this.smoke.setVelocityX(-300);
              this.player.flipX = true;
              this.player.anims.play('jump', true);
+             this.smoke.visible = false;
+             this.smoke.setVelocityY(0);
          } else if (!this.player.body.blocked.down) {
              this.player.setVelocityX(0);
+             this.smoke.setVelocityX(0);
              this.player.anims.play('jump', true);
+             this.smoke.visible = false;
          } else {
              this.player.setVelocityX(0);
+             this.smoke.setVelocityX(0);
              this.player.anims.play('idle', true);
+             this.smoke.visible = false;
          }
      }
 }
@@ -127,7 +172,7 @@ export const useLevelOneScene = () => {
         physics: {
             default: 'arcade',
             arcade: {
-                gravity: { y: 500 },
+                gravity: { y: 1000 },
                 debug: false
             }
         },
