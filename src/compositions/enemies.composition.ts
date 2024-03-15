11aa5a5c3ce30881ab6252267960b46aa11fbd9e
Павел Бездорнов <alpinweb@yandex.ru>
2024-03-15 12:03:39 +0600

    check enemy & visors bounds

--- src/compositions/enemies.composition.ts
+++ src/compositions/enemies.composition.ts
@@ -40,17 +40,26 @@ export const enemiesComposition = {
         layer: Phaser.Tilemaps.TilemapLayer,
         player: Phaser.Physics.Arcade.Image & { body: Phaser.Physics.Arcade.Body },
     ): Phaser.Physics.Arcade.Group{
-        const spawns = map.createFromObjects('Enemies', { gid: 3, key: 'soldier' });
+        const spawns = map.createFromObjects('Enemies', { gid: 4, key: 'soldier' });
+        const visorsSpawns = map.createFromObjects('Enemies', { gid: 4, key: 'visor' });
         const enemies = scene.physics.add.group();
+        const visors = scene.physics.add.staticGroup();
         for (const spawn of spawns) {
-            enemies.add(spawn)
+            enemies.add(spawn);
+            console.log('enemy', spawn.getBounds());
+        }
+        for (const visor of visorsSpawns) {
+            visors.add(visor);
+            console.log('visor', visor.getBounds());
         }
         enemies.children.entries.forEach((enemy) => {
+            enemy.setDisplaySize(138, 138);
             enemy.body.setSize(PLAYER_SIZE.width, 89);
         });
 
         scene.physics.add.collider(player, enemies);
         scene.physics.add.collider(enemies, layer);
+        scene.physics.add.overlap(player, visors);
 
         return enemies;
     },
