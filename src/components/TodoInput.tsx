295b7899790df9fb1a82db08b8ed013195a458e9
Павел Бездорнов <alpinweb@yandex.ru>
2024-03-21 11:54:41 +0600

    remove test components

--- src/components/TodoInput.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TodoInput = (props) => {
-    return (
-        <input {...props} onKeyDown={props.createNewItem} onChange={props.handleChange}/>
-    );
-};
-
-export default TodoInput;
