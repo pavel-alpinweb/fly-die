10cc697edce3b7e4c0f11198d194f33ef468a2d1
Павел Бездорнов <alpinweb@yandex.ru>
2024-04-09 12:00:03 +0600

    edit text

--- src/components/rulesModal/RulesModal.component.tsx
+++ src/components/rulesModal/RulesModal.component.tsx
@@ -16,12 +16,16 @@ const RulesModalComponent = ({isOpen, switchOpenHandler}: RulesModalComponentPro
             <button className={classes.closeBtn} onClick={switchOpenHandler}>
                 <img width={50} src="/assets/ui/CloseBtn.png" alt="MenuBtn"/>
             </button>
-            <h1>Rules:</h1>
+            <h1>Правила:</h1>
             <ol>
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
+                <li>Ваша задача добраться до вершины башни</li>
+                <li>Для перемещения используейте стрелки на клавиатуре либо, клавиши A и D</li>
+                <li>Для полета используйте клаившу "Вверх" на клавиатуре либо клавишу W</li>
+                <li>Для стрельбы используйте клаившу "Пробел"</li>
+                <li>Во время полета тратиться топливо, его можно купить нажав на слот в магазине, либо нажав клавишу 1 на клавиатуре</li>
+                <li>Во время стрельбы тратяться снаряды, их можно купить нажав на слот в магазине, либо нажав клавишу 2 на клавиатуре</li>
+                <li>При поулчении урона, Вы теряете монеты</li>
+                <li>Если получите урон при отсутствии монет, Вы погибните</li>
             </ol>
         </div>
     );
